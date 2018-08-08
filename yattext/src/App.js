065b3e7440fb4fb61126976c556776c0557755d8
Eddie <tamamu.1r1s@gmail.com>
2018-08-08 19:37:48 +0900

    Update

--- yattext/src/App.js
+++ yattext/src/App.js
@@ -3,6 +3,7 @@ import { Provider, connect } from 'react-redux';
 import styled from 'styled-components';
 import Canvas from './atoms/Canvas'
 import Rect from './atoms/Rect'
+import Button from './atoms/Button'
 import Graphic from './atoms/Graphic'
 import Layer from './atoms/Layer'
 import Stage from './atoms/Stage'
@@ -15,40 +16,93 @@ import './App.css';
 
 const store = createFinalState();
 
-
 const timeout = ms => new Promise(r => setTimeout(r, ms))
+
+const rawCommands = [
+  {type: 'text', content: 'Hello, world!'},
+  {type: 'text', content: 'テストです'},
+  {type: 'text', content: '次へ進むにはクリックしてください'},
+  {type: 'command', name: 'l', args: {}},
+  {type: 'text', content: 'クリックされました'},
+  {type: 'text', content: 'テキストレイヤーに指定した行数を超えると自動で改ページされます'},
+  {type: 'text', content: 'クリックしてください'},
+  {type: 'command', name: 'l', args: {}},
+  {type: 'command', name: 'cm', args: {}},
+  {type: 'text', content: '以上です'}
+]
+
+function stringToChars(str) {
+  let chars = []
+  for (let ch of str) {
+    if (ch === '\n') {
+      chars.push({isNewline: true})
+    } else {
+      chars.push({string: ch})
+    }
+  }
+  return chars
+}
+
+function commandToAction(com) {
+  switch (com.name) {
+    case 'l':
+      break;
+    case 'cm':
+      break;
+  }
+}
 
 class App extends Component {
   constructor() {
     super()
     this.forwarding = false
+    this.actions = [];
+    for (let com of rawCommands) {
+      switch (com.type) {
+        case 'text':
+          let content = com.content
+          this.actions.push(TextAction.change(stringToChars(content)))
+          for (let i = 0; i < content.length; i++) {
+            this.actions.push(TextAction.forward())
+          }
+        case 'command':
+          this.actions.push(commandToAction(com))
+          break
+        default:
+          break
+      }
+    }
+    console.log(this.actions)
   }
   changeText() {
     const _chars = [
-      {string:'H'},
-      {string:'e'},
-      {string:'l'},
-      {string:'l'},
-      {string:'o'},
-      {string:','},
+      {string:'H', color: 'red'},
+      {string:'e', color: 'orange'},
+      {string:'l', color: 'yellow'},
+      {string:'l', color: 'lightgreen'},
+      {string:'o', color: 'green'},
+      {string:',', color: 'lightblue'},
       {string:' '},
-      {string:'w'},
-      {string:'o'},
-      {string:'r'},
-      {string:'l'},
-      {string:'d'},
-      {string:'!'},
+      {string:'w', color: 'blue'},
+      {string:'o', color: 'purple'},
+      {string:'r', color: 'black'},
+      {string:'l', color: 'rgba(0,0,0,0.9)'},
+      {string:'d', color: 'rgba(0,0,0,0.8)'},
+      {string:'!', color: 'rgba(0,0,0,0.7)'},
       {isNewline:true},
-      {string:'あ'},
-      {string:'い'},
-      {string:'ろ'},
-      {string:'あ'}
+      {string:'あ', color: 'rgba(0,0,0,0.6)'},
+      {string:'い', color: 'rgba(0,0,0,0.5)'},
+      {string:'ろ', color: 'rgba(0,0,0,0.4)'},
+      {string:'あ', color: 'rgba(0,0,0,0.3)'}
     ]
     store.dispatch(TextAction.change(_chars));
   }
   toggleForwarding() {
     this.forwarding = !this.forwarding
     if (this.forwarding) this.forwardText.bind(this)()
+  }
+  pushed() {
+    console.log("pushed Button component")
   }
   async forwardText() {
     if (this.forwarding) {
@@ -67,6 +121,7 @@ class App extends Component {
                 <Rect color="red" x={12} y={120} width={60} height={60} />
                 <Rect color="green" x={42} y={20} width={160} height={90} />
                 <Graphic src={tamamu} x={0} y={0} width={100} height={100} />
+                <Button label="push me" x={120} y={180} width={200} height={64} onClick={this.pushed} />
               </Layer>
               <Layer width={640} height={100} y={380}>
                 <Text width={640} height={100} />
