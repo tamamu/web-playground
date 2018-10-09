dc8ab7f9cb7dfab71deecf3f31c1833cb4dd2ef9
Eddie <tamamu.1r1s@gmail.com>
2018-10-09 21:34:29 +0900

    Change resolution

--- mars0/src/index.js
+++ mars0/src/index.js
@@ -8,8 +8,8 @@ import MarsZero from "./main"
 window.onload = () => {
   let root = document.querySelector('#root')
   let game = new Game(root, MarsZero)
-  game.size(320, 240)
-  game.screen.style.width="640px";
-  game.screen.style.height="480px";
+  game.size(12*32, 8*32)
+  game.screen.style.width="600px";
+  game.screen.style.height="400px";
   game.mainLoop()
 }
