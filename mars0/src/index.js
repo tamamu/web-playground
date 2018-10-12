5c49cb24aadf5ff708910e829122894620ee7ad8
tamamu <tamamu@irismac.local>
2018-10-12 17:04:00 +0900

    Modify screen size

--- mars0/src/index.js
+++ mars0/src/index.js
@@ -8,8 +8,8 @@ import MarsZero from "./main"
 window.onload = () => {
   let root = document.querySelector('#root')
   let game = new Game(root, MarsZero)
-  game.size(12*32, 8*32)
-  game.screen.style.width="600px";
-  game.screen.style.height="400px";
+  game.size(800, 600)
+  game.screen.style.width="800px";
+  game.screen.style.height="600px";
   game.mainLoop()
 }
