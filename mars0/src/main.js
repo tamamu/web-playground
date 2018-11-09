d2595473931f1396b46ec74ddf87431650d1ddf1
tamamu <tamamu.1r1s@gmail.com>
2018-11-10 04:53:55 +0900

    Add produce window

--- mars0/src/main.js
+++ mars0/src/main.js
@@ -259,24 +259,63 @@ function drawWindow(ctx, x, y, w, h) {
 class ProduceWindow {
   constructor(keystore, inventory) {
     this.keyStore = keystore
+    this.recipes = []
     this.matchedRecipes = []
-    this.usingItems = []
+    this.usingItems = inventory.map(x => null)
     this.inventory = inventory
-    this.selectContent = 0
+    this.selectedItems = inventory.map(x => false)
+    this.selectContent = 1
     this.cursor = 0
     this.closed = false
   }
   input() {
+    if (new Date() - this.keyStore.lastDown < 80) {
+      return 1
+    }
+    this.keyStore.lastDown = new Date()
     const keys = this.keyStore.get()
+    console.log(keys)
     if (keys['ArrowUp']) {
-      this.selectContent = this.selectContent == 0 ? 1 : 0;
+      this.selectContent = this.selectContent - 1
+      if (this.selectContent < 0) {
+        this.selectContent = 2
+      }
     } else if (keys['ArrowDown']) {
-      this.selectContent = this.selectContent == 1 ? 0 : 1;
+      this.selectContent = this.selectContent + 1
+      if (this.selectContent > 2) {
+        this.selectContent = 0
+      }
     }
     if (keys['ArrowLeft']) {
       this.cursor = this.cursor == 0 ? this.inventory.length-1 : this.cursor-1
     } else if (keys['ArrowRight']) {
       this.cursor = this.cursor == this.inventory.length-1 ? 0 : this.cursor+1
+    }
+    if (keys[' ']) {
+      if (this.selectContent == 0) {
+        const itemNum = this.usingItems[this.cursor]
+        if (itemNum != null) {
+          this.selectedItems[itemNum] = false
+          this.usingItems[this.cursor] = null
+        }
+      } else if (this.selectContent == 1) {
+        const item = this.inventory[this.cursor]
+        if (item) {
+          this.selectedItems[this.cursor] = !this.selectedItems[this.cursor]
+          if (this.selectedItems[this.cursor]) {
+            for (let j=0; j < this.usingItems.length; ++j) {
+              if (this.usingItems[j] == null) {
+                this.usingItems[j] = this.cursor
+                break
+              }
+            }
+          } else {
+            this.usingItems = this.usingItems.map(x => x == this.cursor ? null : x)
+          }
+        }
+      } else {
+        console.log("enter")
+      }
     }
     if (keys['Escape']) {
       this.closed = true
@@ -296,20 +335,47 @@ class ProduceWindow {
     return 0
   }
   render(ctx) {
+    ctx.fillStyle = 'rgba(0,0,0,0.7)'
+    ctx.fillRect(0, 0, 800, 600)
     const w = TILESIZE*this.inventory.length
-    drawWindow(ctx, 250, 40, 300, 240)
-    drawWindow(ctx, 400-w/2, 292, w, TILESIZE)
-    drawWindow(ctx, 400-w/2, 352, w, TILESIZE)
+    ctx.fillStyle = 'white'
+    ctx.font = "400 18px 'M PLUS Rounded 1c'"
+    ctx.fillText('材料を選んでください', 310, 12)
+    const y1 = 56
+    const y2 = 312
+    const y3 = 372
+    const y4 = 428
+    drawWindow(ctx, 250, y1, 300, 240)
+    drawWindow(ctx, 400-w/2, y2, w, TILESIZE)
+    drawWindow(ctx, 400-w/2, y3, w, TILESIZE)
+    drawWindow(ctx, 368, y4, 62, 32)
+    ctx.fillStyle = 'white'
+    ctx.fillText("決定", 380, y4+4)
     for (let j=0; j < this.inventory.length; ++j) {
       const item = this.inventory[j]
       if (item) {
-        item.renderable.tiles.render(ctx, item.tileId, TILESIZE * j + 400-w/2, 352, TILESIZE, TILESIZE)
+        if (this.selectedItems[j]) {
+          ctx.fillStyle = 'white'
+          ctx.fillRect(TILESIZE * j + 400-w/2, y3, TILESIZE, TILESIZE)
+        }
+        item.renderable.tiles.render(ctx, item.tileId, TILESIZE * j + 400-w/2, y3, TILESIZE, TILESIZE)
+      }
+    }
+    for (let j=0; j < this.usingItems.length; ++j) {
+      const itemNum = this.usingItems[j]
+      if (itemNum != null) {
+        const item = this.inventory[itemNum]
+        item.renderable.tiles.render(ctx, item.tileId, TILESIZE * j + 400-w/2, y2, TILESIZE, TILESIZE)
       }
     }
     ctx.strokeStyle = 'blue'
-    const x = this.cursor * TILESIZE + 400-w/2
-    const y = this.selectContent == 0 ? 352 : 292
-    ctx.strokeRect(x, y, TILESIZE, TILESIZE)
+    if (this.selectContent < 2) {
+      const x = this.cursor * TILESIZE + 400-w/2
+      const y = this.selectContent == 0 ? y2 : y3
+      ctx.strokeRect(x, y, TILESIZE, TILESIZE)
+    } else {
+      ctx.strokeRect(368, y4, 62, 32)
+    }
   }
 }
 
@@ -580,8 +646,11 @@ export default class MarsZero {
         this.player.direction = direction
         return 0
       }
+    } else if (new Date() - this.keyStore.lastDown < 80) {
+      return 0
     }
     this.keyStore.omit()
+    this.keyStore.lastDown = new Date()
       console.log(this.player.x, this.player.y)
     if (x != 0 || y != 0) {
       if (this.gameMap.collision(this.player.x+x, this.player.y+y) || keys["Alt"]) {
@@ -630,7 +699,7 @@ export default class MarsZero {
     } else if (keys["Escape"]) {
       this.upperWindow = new ProduceWindow(this.keyStore, this.player.stat.itemList)
       this.upperWindowLifeCycle = this.upperWindow.genLifeCycle()
-      return 2
+      return 0
     }
     return 0
   }
@@ -897,6 +966,15 @@ export default class MarsZero {
       PlayerTurn: while (true) {
         switch (this.playerAction()) {
           case 0:
+            while(this.upperWindow && !this.upperWindow.closed) {
+              const v = this.upperWindowLifeCycle.next()
+              if (v == 0) {
+                this.upperWindow = null
+              } else {
+                yield 1
+              }
+            }
+            this.upperWindow = null
             this.ignoreLastInput = true
             yield 3
             break
@@ -904,16 +982,6 @@ export default class MarsZero {
             this.ignoreLastInput = false
             break PlayerTurn
           case 2:
-            if (this.upperWindow) {
-              while(true) {
-                if (this.upperWindowLifeCycle.next()) {
-                  yield 4
-                } else {
-                  break
-                }
-              }
-              this.upperWindow = null
-            }
             this.ignoreLastInput = false
             yield 3
             break PlayerTurn
