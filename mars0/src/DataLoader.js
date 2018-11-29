6f2ed77103126f39738fa1e1e7a8013de6d8ed4d
tamamu <tamamu.1r1s@gmail.com>
2018-11-29 16:42:44 +0900

    Add enemy dictionary

--- mars0/src/DataLoader.js
+++ mars0/src/DataLoader.js
@@ -5,6 +5,10 @@ import FoodState from './Food'
 import {ItemState, Item} from './Item'
 import SeedState from './Seed'
 import WeaponState from './Weapon'
+import {Animatable, Animation, AnimationState, AnimationManager} from "./animation"
+import {CharaStatus, Character} from './Character'
+import {createWalkAnimatable} from './Walker'
+
 
 export class TileDictionary {
   constructor(dir) {
@@ -29,24 +33,8 @@ export class ItemDictionary {
     for (const obj of objList) {
       let instance
       let tm = tileDict.get(obj.img)
-      switch (obj.type) {
-        case 'none':
-          instance = new ItemState(obj.name)
-          break
-        case 'food':
-          instance = new FoodState(obj.name, obj.satiety)
-          break
-        case 'weapon':
-          instance = new WeaponState(obj.name, obj.weaponType, obj.atk, obj.stableRate)
-          break
-        case 'seed':
-          instance = new SeedState(obj.name, obj.subId, obj.periods, obj.nutrition)
-          break
-        default:
-          break
-      }
-      this.dict[obj.id] = {
-        instance, type: obj.type, tile: tm
+     this.dict[obj.id] = {
+        data: obj, type: obj.type, tile: tm
       }
     }
   }
@@ -55,7 +43,46 @@ export class ItemDictionary {
     if (!obj) {
       throw new Error(`Item ${id} has not been registered`)
     }
+    let instance
+    switch (obj.data.type) {
+      case 'none':
+        instance = new ItemState(obj.data.name)
+        break
+      case 'food':
+        instance = new FoodState(obj.data.name, obj.data.satiety)
+        break
+      case 'weapon':
+        instance = new WeaponState(obj.data.name, obj.data.weaponType, obj.data.atk, obj.data.stableRate)
+        break
+      case 'seed':
+        instance = new SeedState(obj.data.name, obj.data.subId, obj.data.periods, obj.data.nutrition)
+        break
+      default:
+        break
+    }
     const renderable = new Renderable(0, 0, obj.tile, 0)
-    return new Item(obj.instance, renderable, 0, 0, 0)
+    return new Item(instance, renderable, 0, 0, 0)
+  }
+}
+
+export class CharaDictionary {
+  constructor(objList, tileDict) {
+    this.src = objList
+    this.dict = {}
+    for (const obj of objList) {
+      let instance = new CharaStatus(obj.name, obj.maxhp, obj.atk, obj.def, obj.lux, obj.exp, true)
+      let tm = tileDict.get(obj.img)
+      this.dict[obj.id] = {
+        instance, tile: tm
+      }
+    }
+  }
+  make(id) {
+    const obj = this.dict[id]
+    if (!obj) {
+      throw new Error(`Item ${id} has not been registered`)
+    }
+    const renderable = new createWalkAnimatable(obj.tile, 0, 0, 1)
+    return new Character(CharaStatus.copy(obj.instance), renderable, 0, 0, 1)
   }
 }
