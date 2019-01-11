ba63de653897e3d3d7d7bf59c565675b71bf8758
tamamu <tamamu.1r1s@gmail.com>
2019-01-11 21:47:06 +0900

    Update

--- mars0/src/game.js
+++ mars0/src/game.js
@@ -1,23 +1,25 @@
 
 export default class Game {
-  constructor(root, generator, options=null) {
+  constructor(root, Generator, options=null) {
     this.screen = document.createElement('canvas')
     this.ctx = this.screen.getContext('2d')
     this.lastRendered = new Date()
     this.width = this.screen.clientWidth
     this.height = this.screen.clientHeight
-    this.generator = new generator(this.ctx, options)
+    this.generator = new Generator(this.ctx, options)
     root.appendChild(this.screen)
   }
   size(width, height) {
-    this.width = this.screen.width = width
-    this.height = this.screen.height = height
+    this.screen.width = width
+    this.width = width
+    this.screen.height = height
+    this.height = height
   }
   mainLoop() {
     this.generator.update()
-    if (new Date - this.lastRendered > 1000/120) {
+    if (Date.now() - this.lastRendered > 1000/120) {
       this.generator.render()
-      this.lastRendered = new Date()
+      this.lastRendered = Date.now()
     }
     requestAnimationFrame(this.mainLoop.bind(this))
   }
