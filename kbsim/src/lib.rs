abc56406a2ae67631af86488aabf608fea6966dc
tamamu <tamamu.1r1s@gmail.com>
2019-03-13 22:31:11 +0900

    Update style

--- kbsim/src/lib.rs
+++ kbsim/src/lib.rs
@@ -31,7 +31,7 @@ fn create_canvas() -> Result<web_sys::HtmlCanvasElement, JsValue> {
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .map_err(|_| ())
         .unwrap();
-    canvas.set_width(640);
+    canvas.set_width(820);
     canvas.set_height(480);
     Ok(canvas)
 }
@@ -43,16 +43,35 @@ fn update_canvas(canvas: &web_sys::HtmlCanvasElement, editor: &Editor) -> Result
         .get_context("2d")?
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
-    context.set_fill_style(&JsValue::from_str("rgba(0, 0, 0, 0.5)"));
+    context.set_fill_style(&JsValue::from_str("rgba(0, 0, 0, 1)"));
     context.clear_rect(0.0, 0.0, width, height);
     context.fill_rect(0.0, 0.0, width, height);
     context.set_text_baseline("top");
-    context.set_fill_style(&JsValue::from_str("black"));
+    context.set_fill_style(&JsValue::from_str("white"));
     context.set_font("20px monospace");
-    context.fill_text(&editor.text, 0.0, 0.0)?;
+    for (i, line) in editor.text.lines().enumerate() {
+        context.fill_text(line, 8.0, 8.0+20.0*i as f64)?;
+    }
+    let mut last_head = 0usize;
+    let mut row = 0;
+    let mut col = 0usize;
+    let mut lpad = 0f64;
+    for (i, ch) in editor.text.chars().enumerate() {
+        if ch == '\n' {
+            row += 1;
+            col = 0;
+            last_head = i+1;
+        } else {
+            col += 1;
+        }
+        if editor.caret_position.row == row && editor.caret_position.col == col {
+            lpad = context.measure_text(&editor.text[last_head..i+1])?.width();
+            break;
+        }
+    }
     context.set_fill_style(&JsValue::from_str("rgba(25, 255, 25, 0.8)"));
-    let lpad = context.measure_text(&editor.text[0..(editor.caret_position.col as usize)])?.width();
-    context.fill_rect(lpad, 0.0, 2.0, 20.0);
+    //let lpad = context.measure_text(&editor.text[0..(editor.caret_position.col as usize)])?.width();
+    context.fill_rect(8.0+lpad, 8.0+20.0*row as f64, 2.0, 20.0);
     Ok(())
 }
 
@@ -63,8 +82,9 @@ fn create_draggable(text: &str) -> Result<web_sys::HtmlElement, JsValue> {
         .map_err(|_| ())
         .unwrap();
     div.set_attribute("draggable", "true")?;
-    div.set_attribute("data-key", text)?;
-    div.set_inner_html(text);
+    div.set_attribute("data-key-btn", text)?;
+    div.set_attribute("data-key-top", text)?;
+    //div.set_inner_html(text);
     Ok(div)
 }
 
@@ -75,7 +95,6 @@ fn create_key_container() -> Result<web_sys::HtmlElement, JsValue> {
         .map_err(|_| ())
         .unwrap();
     div.set_id("key_container");
-    div.style().set_property("width", "600px").unwrap();
     Ok(div)
 }
 
@@ -91,25 +110,18 @@ fn create_style(decl: &str) -> Result<web_sys::HtmlStyleElement, JsValue> {
 
 
 fn apply_style_keytop(elem: &web_sys::HtmlElement) {
-    elem.style().set_property("display", "inline-block").unwrap();
-    elem.style().set_property("width", "36px").unwrap();
-    elem.style().set_property("height", "32px").unwrap();
-    elem.style().set_property("margin", "0px 4px").unwrap();
-    elem.style().set_property("padding-bottom", "6px").unwrap();
-    elem.style().set_property("border", "1px solid black").unwrap();
-    elem.style().set_property("border-radius", "6px").unwrap();
-    elem.style().set_property("text-transform", "uppercase").unwrap();
+    elem.class_list().add_1("key").unwrap();
 }
 
 use std::cell::RefMut;
 fn swap_key(mut km: RefMut<HashMap<String, String>>, key1: &web_sys::HtmlElement, key2: &web_sys::HtmlElement) {
-    let key1_top = key1.inner_html();
-    let key2_top = key2.inner_html();
-    let key1_btn = key1.get_attribute("data-key").unwrap();
-    let key2_btn = key2.get_attribute("data-key").unwrap();
+    let key1_top = key1.get_attribute("data-key-top").unwrap();
+    let key2_top = key2.get_attribute("data-key-top").unwrap();
+    let key1_btn = key1.get_attribute("data-key-btn").unwrap();
+    let key2_btn = key2.get_attribute("data-key-btn").unwrap();
     console_log!("{}(key:{}) <=> {}(key:{})", key1_top, key1_btn, key2_top, key2_btn);
-    key1.set_inner_html(&key2_top);
-    key2.set_inner_html(&key1_top);
+    key1.set_attribute("data-key-top", &key2_top).unwrap();
+    key2.set_attribute("data-key-top", &key1_top).unwrap();
     km.insert(key1_btn, key2_top);
     km.insert(key2_btn, key1_top);
 }
@@ -118,6 +130,7 @@ fn swap_key(mut km: RefMut<HashMap<String, String>>, key1: &web_sys::HtmlElement
 struct CaretPosition {
     row: usize,
     col: usize,
+    count: usize,
 }
 
 #[derive(Clone)]
@@ -128,20 +141,30 @@ struct Editor {
 
 impl Editor {
     fn insert_str(&mut self, s: &str) {
-        self.text.insert_str(self.caret_position.col, s);
+        self.text.insert_str(self.caret_position.count, s);
         self.caret_position.col = self.caret_position.col.saturating_add(s.len());
+        self.caret_position.count = self.caret_position.count.saturating_add(s.len());
+    }
+    fn newline(&mut self) {
+        self.text.insert_str(self.caret_position.count, "\n");
+        self.caret_position.col = 0;
+        self.caret_position.row += 1;
+        self.caret_position.count += 1;
     }
     fn backspace(&mut self) {
         if self.caret_position.col > 0 {
             self.text.replace_range(self.caret_position.col-1..self.caret_position.col, "");
             self.caret_position.col = self.caret_position.col.saturating_sub(1);
+            self.caret_position.count = self.caret_position.count.saturating_sub(1);
         }
     }
     fn move_left(&mut self) {
         self.caret_position.col = self.caret_position.col.saturating_sub(1);
+        self.caret_position.count = self.caret_position.count.saturating_sub(1);
     }
     fn move_right(&mut self) {
         self.caret_position.col = self.caret_position.col.saturating_add(1).min(self.text.len());
+        self.caret_position.count = self.caret_position.count.saturating_add(1).min(self.text.len());
     }
 }
 
@@ -151,9 +174,99 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
     let document = window.document().unwrap();
     let body = document.body().unwrap();
     let style = create_style("
-        #key_container div:nth-child(5) div {
-            width: 100%;
+        body {
+            perspective: 1000px;
         }
+        canvas {
+	        transform: translateY(64px) translateZ(-142px) translateX(200px);
+            border: 10px solid #505254;
+            box-shadow: 0px 2px #333333 inset;
+        }
+        .key.drophover {
+            background: #fafaff;
+            box-shadow: 0 0 25px #e8e8e8 inset, 0 2px 0 #c3c3c3, 0 4px 0 #c9c9c9, 0 4px 6px #333333;
+        }
+        .key {
+            display: inline-block;
+            width: 24px;
+            height: 41px;
+            margin: 5px 6px;
+            padding: 0 10px;
+            border-top: 1px solid #808080;
+            border-bottom: 1px solid #a5a5a5;
+            border-radius: 4px;
+            text-transform: capitalize;
+            color: #aaaaaa;
+            background: #454648;
+            box-shadow: 0 0 25px #e8e8e822 inset, 0 2px 0 #404040, 0 4px 0 #4c4c4c, 0 2px 3px #333333;
+            text-shadow: 0 1px 0 #F5F5F5;
+            transition: background 0.3s;
+        }
+        .key:before {
+            position: absolute;
+            content: attr(data-key-top);
+        }
+        .push.key {
+            border-top: 1px solid #333333;
+            border-bottom: 1px solid #555555;
+            box-shadow: 0 0 25px #e8e8e822 inset, 0 2px 0 transparent, 0 -2px 0 #333, 0 1px 0px #222222 inset;
+        }
+        .push.key:before {
+            margin-top: 4px;
+        }
+        .key[data-key-btn=\"Backspace\"] {
+            width: 80px;
+            text-align: right;
+        }
+        .key[data-key-btn=\"Backspace\"]:before {
+            transform: translateX(-100%);
+        }
+        .key[data-key-btn=\"Tab\"] {
+            width: 56px;
+        }
+        .key[data-key-btn=\"\\\\\"] {
+            width: 48px;
+        }
+        .key[data-key-btn=\"CapsLock\"] {
+            width: 70px;
+        }
+        .key[data-key-btn=\"Enter\"] {
+            width: 90px;
+            text-align: right;
+        }
+        .key[data-key-btn=\"Enter\"]:before {
+            transform: translateX(-100%);
+        }
+        .key[data-key-btn=\"Shift-L\"] {
+            width: 100px;
+        }
+        .key[data-key-btn=\"Shift-R\"] {
+            width: 116px;
+            text-align: right;
+        }
+        .key[data-key-btn=\"Shift-R\"]:before {
+            transform: translateX(-100%);
+        }
+        .key[data-key-btn=\"Ctrl-L\"] {
+            width: 44px;
+        }
+        .key[data-key-btn=\" \"] {
+            width: 248px;
+        }
+
+
+
+
+
+
+
+        #key_container {
+	        transform: rotateX(45deg) translateX(200px);
+            width: 840px;
+            height: 400px;
+            background: #505254;
+        }
+
     ")?;
     let canvas = create_canvas()?;
     let key_container = create_key_container()?;
@@ -167,11 +280,11 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
 */
     let keyboard_model = vec![
-        vec!["1","2","3","4","5","6","7","8","9","0","[","]"],
-        vec!["'",",",".","p","y","f","g","c","r","l","/","=", "\\"],
-        vec!["a","o","e","u","i","d","h","t","n","s","-"],
-        vec![";","q","j","k","x","b","m","w","v","z"],
-        vec![" "]
+        vec!["`","1","2","3","4","5","6","7","8","9","0","[","]","Backspace"],
+        vec!["Tab", "'",",",".","p","y","f","g","c","r","l","/","=", "\\"],
+        vec!["CapsLock","a","o","e","u","i","d","h","t","n","s","-", "Enter"],
+        vec!["Shift-L",";","q","j","k","x","b","m","w","v","z","Shift-R"],
+        vec!["Fn","Ctrl-L","🤔","Alt-L"," ","Alt-R","PrtSc","Ctrl-R"],
     ];
     /*
     let keymap: HashMap<String, String> = [
@@ -201,8 +314,9 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
 
     let editor = Editor {
         text: "Push any key to type text".to_string(),
-        caret_position: CaretPosition {row: 0, col: 0}
+        caret_position: CaretPosition {count: 0, row: 0, col: 0}
         };
+    update_canvas(&canvas, &editor).unwrap();
 
     let drag_src_element: Option<Box<web_sys::HtmlElement>> = None;
     let dse = Rc::new(RefCell::new(drag_src_element));
@@ -240,7 +354,7 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
         {
             let closure = closure!(move |ev: web_sys::DragEvent| {
                 let target = ev.target().unwrap().dyn_into::<web_sys::HtmlElement>().unwrap();
-                target.style().set_property("border", "1px solid red").unwrap();
+                target.class_list().add_1("drophover").unwrap();
                 web_sys::console::log_1(&ev.target().unwrap());
             });
             draggable.add_event_listener_with_callback("dragenter", closure.as_ref().unchecked_ref())?;
@@ -256,7 +370,7 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
         {
             let closure = closure!(move |ev: web_sys::DragEvent| {
                 let target = ev.target().unwrap().dyn_into::<web_sys::HtmlElement>().unwrap();
-                target.style().set_property("border", "1px solid black").unwrap();
+                target.class_list().remove_1("drophover").unwrap();
                 web_sys::console::log_1(&ev);
             });
             draggable.add_event_listener_with_callback("dragleave", closure.as_ref().unchecked_ref())?;
@@ -275,7 +389,7 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
                     Some(elem) => {
                         if !elem.is_same_node(Some(&target)) {
                             swap_key(km1.borrow_mut(), &elem, &target);
-                            target.style().set_property("border", "1px solid black").unwrap();
+                            target.class_list().remove_1("drophover").unwrap();
                         }
                     }
                     _ => {}
@@ -286,27 +400,36 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
         }
     }
 
+    let kt = Rc::new(RefCell::new(keytops));
     let mut editor = Rc::new(editor);
     let canvas = Rc::new(canvas);
     let km1 = km.clone();
+    let kt1 = kt.clone();
     {
         let closure = closure!(move |ev: web_sys::KeyboardEvent| {
             let key = ev.key();
+            ev.prevent_default();
             match &*key {
                 "Backspace" => {
                     make_mut!(editor).backspace();
                 },
                 "ArrowLeft" => {
                     make_mut!(editor).move_left();
-
                 },
                 "ArrowRight" => {
                     make_mut!(editor).move_right();
-                }
+                },
+                "Enter" => {
+                    make_mut!(editor).newline();
+                },
                 _ => {
+                    kt1.borrow().get(&ev.key()).map(|e| e.class_list().add_1("push").unwrap());
                     km1.borrow().get(&ev.key()).map_or_else(
                         || web_sys::console::log_1(&ev),
-                        |k| make_mut!(editor).insert_str(k));
+                        |k| {
+                            ev.prevent_default();
+                            make_mut!(editor).insert_str(k)
+                            });
                 }
             }
             update_canvas(&canvas, &editor).unwrap();
@@ -314,6 +437,15 @@ pub fn greet(name: &str) -> Result<(), JsValue> {
         document.add_event_listener_with_callback("keydown", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
+    let kt1 = kt.clone();
+    {
+        let closure = closure!(move |ev: web_sys::KeyboardEvent| {
+            kt1.borrow().get(&ev.key()).map(|e| e.class_list().remove_1("push").unwrap());
+        });
+        document.add_event_listener_with_callback("keyup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+
     window.alert_with_message(&format!("Keyboard Simulator, {}!", name)).expect("could not alert");
     Ok(())
 }
